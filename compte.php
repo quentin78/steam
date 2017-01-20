e2e7232d4e361b7803b80dee1f9b3578fb2824e3
quentin78 <quentin29norm@gmail.com>
2017-01-20 10:29:26 +0100

    modif page compte.php + details

--- compte.php
+++ compte.php
@@ -4,6 +4,22 @@ include('header.php');
 
 
 
+<div class="profile_header">
+	<div class="profile_header-content">
+		<div  class=avatar_pp>
+			<img src="img/avatar.png" width="100" height="100">
+		</div>
+		<div class=users_name>
+			<h2 class="username"> <?php echo $_SESSION['pseudo']; ?> </h2>
+			<p class="name"> <?php echo $_SESSION['prenom']; ?> </p>
+		</div>
+	</div>
+</div>
+
+
+
+
+
 <?php
 include('footer.php');
 ?>
